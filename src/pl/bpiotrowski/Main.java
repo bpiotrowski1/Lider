c8c6faf1b9fe1953d38e4be8d94dfa1c10144418
bpiotrowski <baej.p26@gmail.com>
2019-06-07 16:26:56 +0200

    final commit

--- src/pl/bpiotrowski/Main.java
+++ src/pl/bpiotrowski/Main.java
@@ -1,8 +1,37 @@
 package pl.bpiotrowski;
 
+import java.util.Scanner;
+
 public class Main {
+    private final static int ARRAY = 500000;
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int[] array = new int[ARRAY];
+
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            zeruj(array);
+
+            for (int j = 0; j < n; j++) {
+                array[in.nextInt()]++;
+            }
+            for (int j = 0; j < ARRAY; j++) {
+                if (array[j] > (n / 2)) {
+                    System.out.println(j);
+                    break;
+                } else if (j == ARRAY - 1) {
+                    System.out.println("-1");
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void zeruj(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 0;
+        }
     }
 }
